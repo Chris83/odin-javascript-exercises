2b786399b51330cfe30857ea25baa12fbffb1b33
Chris83 <83odie@gmail.com>
2022-04-04 21:45:57 -0400

    Completed 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+   text = text.toLowerCase().replace(/[^A-Za-z0-9\s]/g, "").replace(/\s+/g, "");
+   for(let i=0; i<text.length; i++){
+      if(text[i] != text[text.length-1-i])
+       return false;
+   }
+   return true;
 };
 
 // Do not edit below this line
