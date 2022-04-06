9b28351bcf738a815302db0dfa3c2823cd80a681
Chris83 <83odie@gmail.com>
2022-04-05 21:10:26 -0400

    Completed 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   return people.reduce((personA, personB) => {
+      personA.yearOfDeath = personA.yearOfDeath || new Date().getFullYear();
+      personB.yearOfDeath = personB.yearOfDeath || new Date().getFullYear();
+      return personA.yearOfDeath - personA.yearOfBirth > personB.yearOfDeath - personB.yearOfBirth ? personA : personB;
+   });
 };
 
 // Do not edit below this line
